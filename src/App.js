78e28e5b41cc53f5fefc079660f9229a06dc04a0
Mohamed issa <motaouakkel10@gmail.com>
2022-01-27 12:01:24 +0100

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    videos : []
+  }
+  componentDidMount(){
+    axios.get('http://localhost:5000/video').then(res =>{
+        this.setState({videos: res.data}) 
+    })
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <Navbar/>
+      </div>
+    );
+  }
 }
 
 export default App;
